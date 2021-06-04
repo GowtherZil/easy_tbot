b85c494c64d7ba6b051ea93967b3fbe5cdd76ec8
Jesús Enrique <jesusefg12@gmail.com>
2021-06-04 18:20:31 -0400

    added extra  docs

--- easy_tbot/extra/cli/commands/__init__.py
+++ easy_tbot/extra/cli/commands/__init__.py
@@ -1,2 +1,11 @@
+"""
+This module is created as the fragment intended to contain the commands associated 
+with a bot shard.
+"""
+
+#  ##These are the commands that are implemented so far
+
+# This starts the bot up
 from ._run import run
+# and this can be used to create a shard
 from ._create_shard import createshard
--- easy_tbot/extra/create_tbot.py
+++ easy_tbot/extra/create_tbot.py
@@ -1,7 +1,16 @@
+"""
+This module is designed to contain the main entry point associated with 
+easy_tbot allowing us to create the folder structure associated with an 
+easy_tbot project.
+"""
+
+# We import the function to create a project 
 from .tpl import create_project
+# and argparse for handle cli input
 import argparse
 
-
+# ##The entry point
+# This *main* method work as a entry point to create a structure of folders associated with a project
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("name", type=str)
