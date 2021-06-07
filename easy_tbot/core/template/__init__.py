0dec7a7c7e3910a2ad2080a0e2369771161cdb83
Jesús Enrique <jesusefg12@gmail.com>
2021-06-07 10:18:39 -0400

    updated core/template module doc

--- easy_tbot/core/template/__init__.py
+++ easy_tbot/core/template/__init__.py
@@ -5,9 +5,8 @@ engines to lower the complexity and separate the code of control from the messag
 
 # From experience we know that it is more productive to separate the code
 # that represents the logic from the plain text that represents a message.
-# Having a message 40 lines in the middle of our logic would be a nuisance.
-# That is why we include the template module, to separate our logic from plain
-# text.
+# Having a message of 40 lines in the middle of our logic would be a nuisance.
+# That is why we include the template module.
 
 # We import main class (TemplateEngine) as loader to outer scope
 # this class handle all heavy lifting for the template engine
