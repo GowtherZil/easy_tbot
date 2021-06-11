4f21f84efa564d53f80b905376b6041b257951f4
GowtherZil <48606417+GowtherZil@users.noreply.github.com>
2021-06-11 13:10:15 -0400

    Update __init__.py

--- easy_tbot/__init__.py
+++ easy_tbot/__init__.py
@@ -1,5 +1,5 @@
 """
-easy_tbot is a library and a framework for building Telegram Bots. The library is code and
-documented using well curated practices so you should be able to understand wath are we trying
+easy_tbot is a library and framework for building Telegram Bots. The library is coded and
+documented using well curated practices so you should be able to understand what are we trying
 to achieve.
 """
